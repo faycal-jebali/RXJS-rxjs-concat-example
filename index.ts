1f1338417fba5bb3659828f5e89e05d47c007a33
faycal-jebali <39644707+faycal-jebali@users.noreply.github.com>
2023-06-22 23:41:06 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+import './style.css';
+import { concat, empty } from 'rxjs';
+import { delay, startWith } from 'rxjs/operators';
+
+// elems
+const userMessage = document.getElementById('message');
+// helper
+const delayedMessage = (message, delayedTime = 1000) => {
+  return empty().pipe(startWith(message), delay(delayedTime))
+};
+
+concat(
+  delayedMessage('Get Ready!'),
+  delayedMessage(3),
+  delayedMessage(2),
+  delayedMessage(1),
+  delayedMessage('Go!'),
+  delayedMessage('', 2000)
+)
+.subscribe((message: any) => userMessage.innerHTML = message);
